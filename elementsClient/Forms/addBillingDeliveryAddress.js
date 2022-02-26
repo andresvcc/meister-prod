11fd3b227c341a4a76df24cb6b36b014e76e41c8
andresvcc <andres.caballero@etu.unige.ch>
2022-02-26 14:59:35 +0100

    mailRegister

--- elementsClient/Forms/addBillingDeliveryAddress.js
+++ elementsClient/Forms/addBillingDeliveryAddress.js
@@ -4,26 +4,27 @@ import countriesEN from '@/assets/dataBase/DBCountriesEN';
 import countriesFR from '@/assets/dataBase/DBCountriesFR';
 
 const types = {
-  country: {
+  country1: {
     size: [12, 7, 7, 7, 7],
-    type: 'autocomplet',
-    required: true,
-    options: {
-      EN: countriesEN,
-      FR: countriesFR
-    },
+    type: 'input',
     justify: 'center',
+    required: true,
     languages: {
       EN: {
         label: 'Country',
-        info1: 'Email address is required',
-        info2: 'Email address is valid',
-        info3: 'Email address is not valid',
+        info3: 'Correcly filled',
+        info4: 'Country is required'
       },
       FR: {
         label: 'Country',
+        info3: 'Correcly filled',
+        info4: 'Country is required'
       }
     },
+    verify: (formData, currentValue) => [
+      /(?=^.{4,}$)(?=.*[A-Za-z]).*/.test(currentValue) ? { return: true, info: 'info3' } : { return: false, info: 'info4' },
+    ],
+    binding: ['city1']
   },
   zipArea: {
     size: [12, 5, 5, 5, 5],
@@ -39,20 +40,79 @@ const types = {
       }
     },
   },
-
-  zipCode: {
+  city1: {
+    size: [12, 5, 5, 5, 5],
+    type: 'input',
+    justify: 'center',
+    required: true,
+    languages: {
+      EN: {
+        label: 'City',
+        info3: 'Correcly filled',
+        info4: 'City is required'
+      },
+      FR: {
+        label: 'City',
+        info3: 'Correcly filled',
+        info4: 'City is required'
+      }
+    },
+    verify: (formData, currentValue) => [
+      /(?=^.{2,}$)(?=.*[A-Za-z]).*/.test(currentValue) ? { return: true, info: 'info3' } : { return: false, info: 'info4' },
+    ],
+    binding: ['country1']
+  },
+  address1: {
+    size: [12, 7, 7, 7, 7],
+    type: 'input',
+    justify: 'center',
+    required: true,
+    languages: {
+      EN: {
+        label: 'Address',
+        info1: 'The address should have at least 8 letters',
+        info2: 'Invalid address',
+        info3: 'Correcly filled',
+        info4: 'Address is required'
+      },
+      FR: {
+        label: 'Address',
+        info1: 'The address should have at least 8 letters',
+        info2: 'Invalid address',
+        info3: 'Correcly filled',
+        info4: 'Address is required'
+      }
+    },
+    verify: (formData, currentValue) => [
+      /(?=^.{8,}$)(?=.*[A-Za-z]).*/.test(currentValue) ? { return: true, info: 'info3' } : { return: false, info: 'info4' },
+    ],
+    binding: ['postal1']
+  },
+  postal1: {
     size: [12, 5, 5, 5, 5],
     type: 'number',
     justify: 'center',
     required: true,
     languages: {
       EN: {
-        label: 'Postal Code'
+        label: 'Postal Code',
+        info1: 'The postal code should have at least 4 numbers',
+        info2: 'Invalid postal code',
+        info3: 'Correcly filled',
+        info4: 'At least 4 numbers'
       },
       FR: {
-        label: 'Postal Code'
+        label: 'Postal Code',
+        info1: 'The postal code should have at least 4 numbers',
+        info2: 'Invalid postal code',
+        info3: 'Correcly filled',
+        info4: 'Postal Code is required'
       }
     },
+    verify: (formData, currentValue) => [
+      /(?=^.{4,}$)(?=.*[/^\d+$/]).*/.test(currentValue) ? { return: true, info: 'info3' } : { return: false, info: 'info4' },
+    ],
+    binding: ['address1']
   },
   identical: {
     size: [12, 7, 7, 7, 7],
